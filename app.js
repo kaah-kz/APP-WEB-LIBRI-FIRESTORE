89dc39cf585269ad82ab748a3192e088abcf814e
kaah_kz <karolavila850@gmail.com>
2024-04-16 20:29:54 -0300

    INSERÇÃO DE DADOS

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
+/*
+########### LISTAGEM DE LIVROS E AUTORES ###########
+*/
+
+const livrosList = document.querySelector('#book-list');
+
+function renderBook(doc) {
+    //CRIAÇÃO DOS ELEMENTOS HTML:
+    let li = document.createElement('li');
+    let titulo = document.createElement('span');
+    let autor = document.createElement('span');
+
+    //CARREGA OS DADOS NOS ELEMENTOS HTML:
+    li.setAttribute('data-id', doc.id);
+    titulo.textContent = doc.data().titulo;
+    autor.textContent = doc.data().autor;
+
+    //ADICIONANDO OS DADOS DE AUTOR E TÍTULO NA TAG LI:
+    li.appendChild(titulo);
+    li.appendChild(autor);
+
+    //ADICIONANDO O LI NA TAG UL:
+    livrosList.appendChild(li);
+}
+
 db.collection('libri-firestore')
     .get()
     .then(
@@ -5,6 +30,28 @@ db.collection('libri-firestore')
             //console.log(snapshot.docs)
             snapshot.docs.forEach(doc => {
                 console.log(doc.data());
+                renderBook(doc);
             });
         }
-    )
+    );
+
+/*
+########### INSERÇÃO DE LIVROS E AUTORES ###########
+*/
+
+const form = document.querySelector('#add-book-form');
+
+form.addEventListener('submit', (event)=>{
+
+    event.preventDefault();
+
+    db.collection('libri-firestore').add({
+        autor: form.autor.value,
+        titulo: form.titulo.value
+    }).then(()=>{
+        form.autor.value = '';
+        form.titulo.value = '';
+        window.location.reload();
+    });
+
+});
